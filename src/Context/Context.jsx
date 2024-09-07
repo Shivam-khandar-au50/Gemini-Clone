d4c94f6b7d46e02f85be841d7f9dddccb5c89316
Shivam-khandar-au50 <ahivam.khandar@gmail.com>
2024-09-07 18:58:57 +0530

    GEMINI CLONE

---
+++ src/Context/Context.jsx
@@ -0,0 +1,82 @@
+import { createContext, useState } from "react";
+import run from "../Config/Gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setResentPrompt] = useState("");
+  const [previousPrompt, setPreviousPrompt] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+
+  const delaypara = (index, nextWord) => {
+    setTimeout(function () {
+      setResultData((prev) => prev + nextWord);
+    }, 75 * index);
+  };
+  const newChat = () => {
+    setLoading(false)
+    setShowResult(false)
+  }
+  const onSent = async (prompt) => {
+    console.log(input);
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    let response;
+    if(prompt !==undefined && typeof prompt === "string"){
+      response = await run(prompt);
+      setResentPrompt(prompt);
+    }else{
+      setPreviousPrompt((prev) => [...prev, input]);
+      setResentPrompt(input)
+      response = await run(input);
+    }
+  
+
+    let responseArray = response.split("**");
+    let newResponse = "";
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newResponse += responseArray[i];
+      } else {
+        newResponse += "<b>" + responseArray[i] + "</b>";
+      }
+    }
+    let newResponse2 = newResponse.split("*").join("</br>");
+    let newResponseArray = newResponse2.split(" ");
+    for (let i = 0; i < newResponseArray.length; i++) {
+      const nextword = newResponseArray[i];
+      delaypara(i, nextword + " ");
+    }
+
+    // setResultData(newResponse2);
+    setLoading(false);
+    setInput("");
+  };
+
+  //   onSent("what is react js")
+  const contextValue = {
+    previousPrompt,
+    setPreviousPrompt,
+    onSent,
+    setResentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+    newChat
+  };
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
+
+export default ContextProvider;
+
+// join the video in 1:32;47
